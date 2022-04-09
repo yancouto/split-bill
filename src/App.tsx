8cdd84dd33a34cbed18f9535a45d1cc634030ea0
Gabriel Russo <gabriel.russo@databricks.com>
2022-04-09 15:59:28 +0200

    reformat everything with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import {
-  IonApp,
-} from '@ionic/react';
-import Start from './pages/Start';
-import Split from './pages/Split';
+import { IonApp } from "@ionic/react";
+import Start from "./pages/Start";
+import Split from "./pages/Split";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
-import { useState } from 'react';
-import { ChangePage, PageState, PageType } from './pages/types';
-import Expense from './pages/Expense';
+import "./theme/variables.css";
+import { useState } from "react";
+import { ChangePage, PageState, PageType } from "./pages/types";
+import Expense from "./pages/Expense";
 
 /* Dinero.js defaults */
-import Dinero from 'dinero.js'
-Dinero.defaultCurrency = 'USD'
-Dinero.defaultPrecision = 2
+import Dinero from "dinero.js";
+Dinero.defaultCurrency = "USD";
+Dinero.defaultPrecision = 2;
 
 const App: React.FC = () => {
   const [page, setPage] = useState<PageState>(() => ({ type: PageType.Start }));
@@ -41,11 +39,11 @@ const App: React.FC = () => {
   } else if (page.type === PageType.Expense) {
     element = <Expense state={page} />;
   }
-  return <IonApp>
-    <ChangePage.Provider value={setPage}>
-      {element}
-    </ChangePage.Provider>
-  </IonApp>;
-}
+  return (
+    <IonApp>
+      <ChangePage.Provider value={setPage}>{element}</ChangePage.Provider>
+    </IonApp>
+  );
+};
 
 export default App;
--- src/pages/Expense.tsx
+++ src/pages/Expense.tsx
@@ -1,92 +1,136 @@
-import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonItemDivider, IonLabel, IonList, IonPage, IonTitle } from '@ionic/react';
-import { useContext, useState } from 'react';
-import { parseToFloat } from '../common/utils';
-import { derive } from './Split';
-import './Start.css';
-import { ChangePage, ExpenseState, PageType } from './types';
-import Dinero from 'dinero.js';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonItemDivider,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+} from "@ionic/react";
+import { useContext, useState } from "react";
+import { parseToFloat } from "../common/utils";
+import { derive } from "./Split";
+import "./Start.css";
+import { ChangePage, ExpenseState, PageType } from "./types";
+import Dinero from "dinero.js";
 
 interface Props {
-    state: ExpenseState
+  state: ExpenseState;
 }
 
 const Expense: React.FC<Props> = (props) => {
-    const derived = derive(props.state.split);
-    const [price, setPrice] = useState<Dinero.Dinero>(Dinero());
-    const [sharesPerPerson, setSharesPerPerson] = useState<Map<number, number>>(() => new Map());
-    const totalShares = [...sharesPerPerson.values()].reduce((acc, cur) => acc + cur, 0);
-    const valid = price.isPositive() && totalShares > 0 && price.lessThanOrEqual(derived.left);
-    const changePage = useContext(ChangePage);
+  const derived = derive(props.state.split);
+  const [price, setPrice] = useState<Dinero.Dinero>(Dinero());
+  const [sharesPerPerson, setSharesPerPerson] = useState<Map<number, number>>(
+    () => new Map()
+  );
+  const totalShares = [...sharesPerPerson.values()].reduce(
+    (acc, cur) => acc + cur,
+    0
+  );
+  const valid =
+    price.isPositive() &&
+    totalShares > 0 &&
+    price.lessThanOrEqual(derived.left);
+  const changePage = useContext(ChangePage);
 
-    function componentForPerson(name: string, idx: number) {
-        const shares = sharesPerPerson.get(idx) ?? 0;
-        let part;
-        if (totalShares > 0 && price.isPositive()) {
-            part = <IonLabel>~ ${price.multiply(shares).divide(totalShares).toUnit()}</IonLabel>;
-        }
-        return <IonItem key={idx}>
-            <IonLabel>{name}:</IonLabel>
-            <IonInput type="number" inputmode="numeric" placeholder="0"
-                value={shares}
-                onIonChange={e => {
-                    const newShares = new Map(sharesPerPerson);
-                    newShares.set(idx, parseToFloat(e.detail.value));
-                    setSharesPerPerson(newShares);
-                }} ></IonInput>
-            {part}
-        </IonItem>;
+  function componentForPerson(name: string, idx: number) {
+    const shares = sharesPerPerson.get(idx) ?? 0;
+    let part;
+    if (totalShares > 0 && price.isPositive()) {
+      part = (
+        <IonLabel>
+          ~ ${price.multiply(shares).divide(totalShares).toUnit()}
+        </IonLabel>
+      );
     }
-
-    function saveExpense() {
-        const idxToValue: Map<number, Dinero.Dinero> = new Map();
-        for (const [idx, shares] of sharesPerPerson) {
-            idxToValue.set(idx, price.multiply(shares).divide(totalShares));
-        }
-        const split = props.state.split;
-        changePage({
-            type: PageType.Split,
-            split: {
-                all_items: [...split.all_items, { per_person: idxToValue }],
-                names: split.names,
-                total: split.total,
-            },
-        });
-    }
-
-    function cancel() {
-        changePage({
-            type: PageType.Split,
-            split: props.state.split,
-        });
-    }
-
     return (
-        <IonPage>
-            <IonHeader>
-                <IonTitle>Split Bill</IonTitle>
-            </IonHeader>
-            <IonContent fullscreen>
-                <IonList>
-                    <IonItem>
-                        <IonLabel>Price:</IonLabel>
-                        <IonInput type="number" inputmode="numeric" placeholder="0"
-                            value={price.toUnit()}
-                            onIonChange={e => setPrice(Dinero({ amount: 100 * parseToFloat(e.detail.value) }))}></IonInput>
-                    </IonItem>
-                    <IonItemDivider>
-                        <IonLabel>Shares per person</IonLabel>
-                    </IonItemDivider>
-                    {props.state.split.names.map(componentForPerson)}
-                    <IonItem>
-                        <IonButton disabled={!valid} expand="block" size="large" onClick={saveExpense}>Save expense</IonButton>
-                    </IonItem>
-                    <IonItem>
-                        <IonButton expand="block" size="large" onClick={cancel}>Delete expense</IonButton>
-                    </IonItem>
-                </IonList>
-            </IonContent>
-        </IonPage>
+      <IonItem key={idx}>
+        <IonLabel>{name}:</IonLabel>
+        <IonInput
+          type="number"
+          inputmode="numeric"
+          placeholder="0"
+          value={shares}
+          onIonChange={(e) => {
+            const newShares = new Map(sharesPerPerson);
+            newShares.set(idx, parseToFloat(e.detail.value));
+            setSharesPerPerson(newShares);
+          }}
+        ></IonInput>
+        {part}
+      </IonItem>
     );
+  }
+
+  function saveExpense() {
+    const idxToValue: Map<number, Dinero.Dinero> = new Map();
+    for (const [idx, shares] of sharesPerPerson) {
+      idxToValue.set(idx, price.multiply(shares).divide(totalShares));
+    }
+    const split = props.state.split;
+    changePage({
+      type: PageType.Split,
+      split: {
+        all_items: [...split.all_items, { per_person: idxToValue }],
+        names: split.names,
+        total: split.total,
+      },
+    });
+  }
+
+  function cancel() {
+    changePage({
+      type: PageType.Split,
+      split: props.state.split,
+    });
+  }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonTitle>Split Bill</IonTitle>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonList>
+          <IonItem>
+            <IonLabel>Price:</IonLabel>
+            <IonInput
+              type="number"
+              inputmode="numeric"
+              placeholder="0"
+              value={price.toUnit()}
+              onIonChange={(e) =>
+                setPrice(Dinero({ amount: 100 * parseToFloat(e.detail.value) }))
+              }
+            ></IonInput>
+          </IonItem>
+          <IonItemDivider>
+            <IonLabel>Shares per person</IonLabel>
+          </IonItemDivider>
+          {props.state.split.names.map(componentForPerson)}
+          <IonItem>
+            <IonButton
+              disabled={!valid}
+              expand="block"
+              size="large"
+              onClick={saveExpense}
+            >
+              Save expense
+            </IonButton>
+          </IonItem>
+          <IonItem>
+            <IonButton expand="block" size="large" onClick={cancel}>
+              Delete expense
+            </IonButton>
+          </IonItem>
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Expense;
